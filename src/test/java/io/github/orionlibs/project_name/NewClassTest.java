29c2a682909bd5b603a97422f6d0562b1b2495d4
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-09 19:18:18 +0100

    updated

--- src/test/java/io/github/orionlibs/project_name/NewClassTest.java
+++ src/test/java/io/github/orionlibs/project_name/NewClassTest.java
@@ -1,16 +1,10 @@
 package io.github.orionlibs.project_name;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 import io.github.orionlibs.project_name.config.FakeTestingSpringConfiguration;
-import io.github.orionlibs.project_name.config.MockController;
 import io.github.orionlibs.project_name.log.ListLogHandler;
-import java.io.IOException;
-import java.util.logging.LogManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,8 +12,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("testing")
@@ -29,25 +21,15 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 public class NewClassTest
 {
     private ListLogHandler listLogHandler;
-    private MockMvc mockMvc;
+    //@Autowired
+    //private NewClass newClass;
 
 
     @BeforeEach
     void setUp()
     {
-        try
-        {
-            listLogHandler = new ListLogHandler();
-            LogManager.getLogManager().readConfiguration(NewClassTest.class.getResourceAsStream("/io/github/orionlibs/project-name/configuration/orion-logger.prop"));
-            NewClass.addLogHandler(listLogHandler);
-            mockMvc = MockMvcBuilders
-                            .standaloneSetup(new MockController())
-                            .build();
-        }
-        catch(IOException e)
-        {
-            System.err.println("Could not setup logger configuration for Orion project-name: " + e.toString());
-        }
+        listLogHandler = new ListLogHandler();
+        NewClass.addLogHandler(listLogHandler);
     }
 
 
@@ -61,10 +43,8 @@ public class NewClassTest
     @Test
     void test_method1() throws Exception
     {
-        //ConfigurationService.updateProp("orionlibs.prop", "false");
-        mockMvc.perform(get("/")).andExpect(status().isOk());
-        assertTrue(listLogHandler.getLogRecords().stream()
-                        .anyMatch(record -> record.getMessage().contains("hello world")));
-        //ConfigurationService.updateProp("orionlibs.prop", "true");
+        //do something
+        //assertTrue(listLogHandler.getLogRecords().stream()
+        //                .anyMatch(record -> record.getMessage().contains("hello world")));
     }
 }
--- src/test/java/io/github/orionlibs/project_name/config/FakeTestingSpringConfiguration.java
+++ src/test/java/io/github/orionlibs/project_name/config/FakeTestingSpringConfiguration.java
@@ -1,8 +1,11 @@
 package io.github.orionlibs.project_name.config;
 
+import java.io.IOException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.core.env.Environment;
 
 public class FakeTestingSpringConfiguration
 {
@@ -13,5 +16,20 @@ public class FakeTestingSpringConfiguration
                     {"io.github.orionlibs"})
     public static class FakeConfiguration
     {
+        private Environment springEnv;
+
+
+        @Autowired
+        public FakeConfiguration(final Environment springEnv) throws IOException
+        {
+            this.springEnv = springEnv;
+        }
+
+
+        /*@Bean
+        public NewClass newClass()
+        {
+            return new NewClass(springEnv);
+        }*/
     }
 }
