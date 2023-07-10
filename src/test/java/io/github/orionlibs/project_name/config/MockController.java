33720adbef76e3a4b99066b6a618552b59f6f5b6
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2023-07-10 18:40:25 +0100

    updated

--- src/test/java/io/github/orionlibs/project_name/config/MockController.java
+++ src/test/java/io/github/orionlibs/project_name/config/MockController.java
@@ -1,7 +1,7 @@
 package io.github.orionlibs.project_name.config;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
